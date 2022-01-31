e583f21f1c19c8363919c9b7c55e57d42f350f3c
Prince Junior Mguni <prince.mguni@outlook.com>
2022-01-31 11:47:01 +0200

    added vuestic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 import { createApp } from "vue";
+import { VuesticPlugin } from "vuestic-ui"; // <-
+import "vuestic-ui/dist/vuestic-ui.css"; // <-
 import App from "./App.vue";
 import router from "./router";
 
 setTimeout(function () {
-  createApp(App).use(router).mount("#app");
+  createApp(App)
+    .use(router, VuesticPlugin, {
+      colors: {
+        // Default colors
+        primary: "#23e066",
+        secondary: "#002c85",
+        success: "#40e583",
+        info: "#2c82e0",
+        danger: "#e34b4a",
+        warning: "#ffc200",
+        gray: "#babfc2",
+        dark: "#34495e",
+
+        // Custom colors
+        myprimary: "#FF5722",
+        mysecondary: "#FFCCBC",
+        myaccent: "#2e302f",
+        myerror: "#fe2a39",
+      },
+    })
+    .mount("#app");
 }, 3400);
--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,10 +1,57 @@
-<template></template>
+<template>
+  <div id="container">
+    <va-navbar color="primary" class="mb-2">
+      <template #left>
+        <va-navbar-item>LOGO HERE</va-navbar-item>
+      </template>
+      <template #center>
+        <va-navbar-item>Primary</va-navbar-item>
+      </template>
+      <template #right>
+        <va-navbar-item>Auto font color</va-navbar-item>
+      </template>
+    </va-navbar>
+  </div>
+</template>
 
 <script>
+import myMessages from "@/assets/codingTest/messages.json";
+
 export default {
   name: "Messages",
+  data: () => {
+    return {
+      messages: myMessages,
+    };
+  },
 };
 </script>
 
 <style>
+ul {
+  list-style-type: none;
+}
+.collapsible {
+  background-color: #777;
+  color: white;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+.active,
+.collapsible:hover {
+  background-color: #555;
+}
+
+.content {
+  padding: 0 18px;
+  display: none;
+  overflow: hidden;
+  background-color: #f1f1f1;
+}
 </style>
